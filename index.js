13999c407605404faad690fb6c448ef78586e114
khalid elaggan <khalid.elaggan@hotmail.com>
2017-03-27 16:33:00 -0400

    Alexa Draft App1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var sampleData = require('./sample.json')
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send(sampleData)
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
